3c112530355c3874f75bc507d954cecd5091ba78
jazmintsakoumagkos <jazmintsakoumagkos@gmail.com>
2024-12-19 18:04:37 -0300

    variables de entorno

--- src/index.js
+++ src/index.js
@@ -7,18 +7,20 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const noteRoutes = require('./routes/note');
 
+// Cargar las variables de entorno
+require('dotenv').config(); 
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
-const port = 3000;
-
+const port = process.env.PORT || 3000; // Usar el puerto de la variable de entorno, si no está disponible, usar 3000.
 
 const dbconnect = async () => {
   try {
     mongoose.set('strictQuery', true); 
-    await mongoose.connect('mongodb://127.0.0.1:27017/lista_tareas'); 
+    await mongoose.connect(process.env.MONGO_URI); // Usar la URI de MongoDB desde las variables de entorno
+    console.log('Conexión correcta a la base de datos');
   } catch (error) {
     console.error('Error de conexión a la base de datos:', error);
     process.exit(1); 
@@ -26,7 +28,6 @@ const dbconnect = async () => {
 };
 
 dbconnect();
-
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -39,9 +40,7 @@ app.use((req, res, next) => {
   next();
 });
 
-
 app.use('/api', noteRoutes);
-
 
 app.listen(port, () => {
   console.log(`Servidor ejecutándose en el puerto ${port}`);
